37102678f813cf317372ce4d83af77b2ebd08794
Mitch Faber <faberskits@gmail.com>
2021-11-24 12:36:20 -0400

    need to implement router, then navbar is done.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import {React, useState} from 'react'
+import {Link} from 'react-router-dom'
 
 function App() {
+    const [activePage, setActivePage] = useState('Home')
+
     return (
-        <div class="container">
-            <h1>Faber Pizza</h1>
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container">
+                    <a className="navbar-brand" href="/">Faber Pizza</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link className={activePage === "Home" ? "nav-link active" : "nav-link"} to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/order">Menu/Online Order</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>    
     );
 }
